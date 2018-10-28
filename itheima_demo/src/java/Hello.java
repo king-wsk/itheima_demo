651f8743839deb7cd0f54b3e195d7ab8ecc1ecfc
King-wsk <king_wsk@163.com>
2018-10-28 20:50:45 +0800

    更新 Hello.java

--- itheima_demo/src/java/Hello.java
+++ itheima_demo/src/java/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
         System.out.println("你好");
         System.out.println("你好");
         System.out.println("你好");
-
+            //加了一行注释
         int i = 0;
         i = i + 10;
         System.out.println(i);
