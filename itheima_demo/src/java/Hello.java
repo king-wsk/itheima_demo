6d9d386e5abadafab3c0dbfb962a905d60a252ef
King <King_Wsk>
2018-10-28 20:37:43 +0800

    上传itheima_demo项目

---
+++ itheima_demo/src/java/Hello.java
@@ -0,0 +1,16 @@
+package java;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
+
+        int i = 0;
+        i = i + 10;
+        System.out.println(i);
+    }
+}
